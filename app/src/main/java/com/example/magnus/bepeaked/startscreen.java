80694bb105220021af06f2be2779c6dd4761c3c2
IamHexor <ykhexor@hotmail.com>
2016-10-10 10:43:24 +0200

    test

--- app/src/main/java/com/example/magnus/bepeaked/startscreen.java
+++ app/src/main/java/com/example/magnus/bepeaked/startscreen.java
@@ -9,5 +9,7 @@ public class startscreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startscreen);
+
+        // arne han er for cool
     }
 }
